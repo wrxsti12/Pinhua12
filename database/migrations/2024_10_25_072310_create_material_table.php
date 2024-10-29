d938a89c6d0a7807e92df7d1dda0f7772d4ae0ea
D1040 <123>
2024-10-29 11:39:26 +0800

    1029

--- database/migrations/2024_10_25_072310_create_material_table.php
+++ database/migrations/2024_10_25_072310_create_material_table.php
@@ -4,33 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMaterialTable extends Migration
+class CreateMaterialsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migration.
      *
      * @return void
      */
     public function up()
     {
-        Schema::create('material', function (Blueprint $table) {
-            $table->id();
-            $table->string('name'); // 名稱
-            $table->decimal('quantity', 8, 4); // 數量
-            $table->string('unit'); // 單位
-            $table->string('department_name'); // 部門名稱
-            $table->year('announcement_year'); // 公告年份
-            $table->timestamps(); // 建立及更新時間
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();                              // 自增主鍵
+            $table->string('name')->comment('物料名稱');                    // 物料名稱
+            $table->decimal('coe', 10, 4)->comment('係數');              // 係數
+            $table->string('unit')->comment('單位');                      // 單位
+            $table->string('department_name')->comment('部門名稱');          // 部門名稱
+            $table->integer('announcement_year')->comment('公告年份');       // 公告年份
+            $table->timestamps();  // 生成 created_at 和 updated_at 欄位
         });
     }
 
     /**
-     * Reverse the migration.
+     * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('material');
+        Schema::dropIfExists('materials');
     }
 }
