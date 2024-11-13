45432720df4d8a8c6a97cc27f4bb78e5dfee477c
wrxsti12 <nf3xedk12@gmail.com>
2024-11-13 11:35:38 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MaterialsObservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('Observation', [MaterialsObservationController::class, 'index']);
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    '/sgds',
+     function () {
+    return view('intro_sdgs');
+});
+
